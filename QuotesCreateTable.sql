7bfd6357db12d43dda4be58e346de096af549e52
Nicolette <anderson.nicolette.r@gmail.com>
2024-11-11 15:28:45 -0600

    added comments

--- QuotesCreateTable.sql
+++ QuotesCreateTable.sql
@@ -1,3 +1,7 @@
+/* This demonstrates creating a table with an integer primary key that autoincrements by 1, and starts at 1. 
+	QuoteText and QuoteAuthor are the only fields that are updated by the user. 
+	Setting DEFAULT(getDate()) on CreatedDate and UpdatedDate will automatically update these fields with the current date and time
+*/
 create table Quotes (  
 	QuoteId int IDENTITY(1, 1) NOT NULL primary key,  
 	QuoteText varchar(255),  
